32b6a0583af22e24321aa0f666ac9793b2824d23
p@rick <frzpete@gmail.com>
2019-03-25 15:56:57 +0300

    chore(django): Upgrade Authors heaven to django 2.1
    - Update and add application dependencies to requirements.txt such as django-extensions, django-cors-headers
    - Replace url with path method
    - Replace django.conf.urls with django.urls
    - Remove reference to non existent authentication in backend
    
    [Delivers #164798278]

--- authors/settings.py
+++ authors/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'corsheaders',
     'django_extensions',
     'rest_framework',
@@ -135,9 +134,5 @@ AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
-    'NON_FIELD_ERRORS_KEY': 'error',
-
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authors.apps.authentication.backends.JWTAuthentication',
-    ),
+    'NON_FIELD_ERRORS_KEY': 'error'
 }
