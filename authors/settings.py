9a26bfb1fc6987182d93aae9387ecac25ab3ab18
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2024-04-10 19:11:59 +0300

    chore:  change db port env name to avoid duplicate names

--- authors/settings.py
+++ authors/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
